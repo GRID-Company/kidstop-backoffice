86c6e8f03a19c70d7fd8aaef8d5872c040e2478d
Carlos Alonso <57297487+carlos-alonso-al@users.noreply.github.com>
2026-02-08 18:16:11 -0600

    feat: add users domain types, constants and domain logic (#6)

---
+++ src/features/users/domain/users.domain.ts
@@ -0,0 +1,29 @@
+import { FindUsersArgs } from '@/lib/api/schema-types';
+import { IPaginatedApiArgs } from '@/lib/types/datatable.types';
+import { USER_ROLES } from './constants';
+import { UserFilters, UserRole } from './types';
+
+export const getUsersVars = (
+  args: IPaginatedApiArgs,
+  filters?: UserFilters
+): { findUsersArgs: FindUsersArgs } => {
+  return {
+    findUsersArgs: {
+      skip: args.skip,
+      limit: args.limit,
+      sort: {
+        column: args.sort.column,
+        order: args.sort.order,
+      },
+      search: args.search,
+      filters: {
+        role: filters?.role,
+        activated: filters?.activated,
+      },
+    },
+  };
+};
+
+export const validateUserRole = (role: string): role is UserRole => {
+  return Object.values(USER_ROLES).includes(role as UserRole);
+};
